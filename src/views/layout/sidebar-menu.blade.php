7db93fd6db03a45cbe3abd4c19146950f897c84e
Muhammad Syifa <emsifa@gmail.com>
2017-10-09 17:54:00 +0700

    Add menu child in sidebar

--- src/views/layout/sidebar-menu.blade.php
+++ src/views/layout/sidebar-menu.blade.php
@@ -1,11 +1,33 @@
 <ul class="sidebar-menu">
   <li class="header">MAIN NAVIGATION</li>
   @foreach(config('{? config_key ?}.menu') as $menu)
-  <li class="{{ Request::route()->getName() == $menu['route']? 'active' : '' }}">
-    <a href="{{ route($menu['route']) }}">
-      <i class="fa {{ $menu['icon'] }}"></i>
-      <span>{{ $menu['label'] }}</span>
-    </a>
-  </li>
+    @if(isset($menu['child']) AND is_array($menu['child']))
+    <li class="treeview">
+      <a href="#">
+        <i class="fa {{ $menu['icon'] }}"></i> <span>{{ $menu['label'] }}</span>
+        <span class="pull-right-container">
+          <i class="fa fa-angle-left pull-right"></i>
+        </span>
+      </a>
+      <ul class="treeview-menu">
+        @foreach($menu['child'] as $childmenu)
+        <li class="{{ Request::route()->getName() == $childmenu['route']? 'active' : '' }}">
+          <a href="{{ route($childmenu['route']) }}">
+            <i class="fa {{ $childmenu['icon'] }}"></i>
+            <span>{{ $childmenu['label'] }}</span>
+          </a>
+        </li>
+        @endforeach
+      </ul>
+    </li>
+    @else
+    <li class="{{ Request::route()->getName() == $menu['route']? 'active' : '' }}">
+      <a href="{{ route($menu['route']) }}">
+        <i class="fa {{ $menu['icon'] }}"></i>
+        <span>{{ $menu['label'] }}</span>
+      </a>
+    </li>
+    @endif
   @endforeach
 </ul>
+
